e96ff4589b0e22246c171e4cf0c661b6759462ee
Divyanshi0307 <127880968+Divyanshi0307@users.noreply.github.com>
2023-12-23 01:04:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// alert("hello");
+// let imgBox = document.getElementById("imgBox");
+// let qrImage = document.getElementById("qrImage");
+// let qrText = document.getElementById("grText");
+
+// function generateQR()
+// {
+//     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+// }
